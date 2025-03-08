9d21aabd170a4207ee8df1d0b8832f36e38631be
akononenko <akononenko@pushwoosh.com>
2025-03-09 00:24:21 +0300

    Autoconfigs are added to iOS and Android

--- plugin/src/withPushwooshIOS.ts
+++ plugin/src/withPushwooshIOS.ts
@@ -6,7 +6,7 @@ import {
 
 import { PushwooshIOSPluginProps } from './withPushwoosh';  
 
-const withCapabilities: ConfigPlugin<PushwooshIOSPluginProps> = (config, props) => {
+const withCapabilities: ConfigPlugin = (config) => {
   return withInfoPlist(config, (plist) => {
     if (!Array.isArray(plist.modResults.UIBackgroundModes)) {
       plist.modResults.UIBackgroundModes = [];
@@ -15,19 +15,27 @@ const withCapabilities: ConfigPlugin<PushwooshIOSPluginProps> = (config, props) 
     if (!plist.modResults.UIBackgroundModes.includes("remote-notification")) {
       plist.modResults.UIBackgroundModes.push("remote-notification");
     }
+    
     return plist;
   });
 };
 
-const withAPNSEnvironment: ConfigPlugin<PushwooshIOSPluginProps> = (config, props) => {
+const withAPNSEnvironment: ConfigPlugin<{ mode: string }> = (config, props) => {
   return withEntitlementsPlist(config, (plist) => {
     plist.modResults['aps-environment'] = props.mode
     return plist;
   });
 };
 
-export const withPushwooshIOS: ConfigPlugin<PushwooshIOSPluginProps> = (config, props) => {
-  config = withCapabilities(config, props)
+export const withPushwooshIOS: ConfigPlugin<{ mode: string } & PushwooshIOSPluginProps> = (config, props) => {
+  config = withCapabilities(config)
+  
+  config = withInfoPlist(config, (plist) => {
+    plist.modResults['PW_API_TOKEN'] = props?.PW_API_TOKEN || '__YOUR_API_TOKEN__';
+    plist.modResults['Pushwoosh_LOG_LEVEL'] = props?.Pushwoosh_LOG_LEVEL || 'INFO';
+    return plist;
+  });
+  
   config = withAPNSEnvironment(config, props)
   return config;
 };
